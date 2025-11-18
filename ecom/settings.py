4815201ff872bfe4017d4dd2338ed31dda9af8bb
Hani <hanider27@gmail.com>
2025-11-18 23:55:43 +0100

    mise a jours

--- ecom/settings.py
+++ ecom/settings.py
@@ -151,8 +151,8 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_PUBLIC_KEY')
-RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_PRIVATE_KEY')
+RECAPTCHA_PUBLIC_KEY = '6LcEngosAAAAAKGEuKQnlNbn2ZQNsHultxJw7sD1'
+RECAPTCHA_PRIVATE_KEY = '6LcEngosAAAAAEWdS3q3YQEeLMAn1UbZXEym9lTs'
 
 #Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
