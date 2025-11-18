0ee6fe6cf95736b2f11eb93e0156dd705c851480
Hani <hanider27@gmail.com>
2025-11-19 00:20:42 +0100

    mise a jours

--- ecom/settings.py
+++ ecom/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': 'hopper.proxy.rlwy.net',  
         'PORT': '47300',
     }
