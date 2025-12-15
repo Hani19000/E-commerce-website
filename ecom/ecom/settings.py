2622bc97bbc2743cb65efd7635c530bcd9972eb7
Hani <hanider27@gmail.com>
2025-12-15 17:48:52 +0100

    mise a jours

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -91,28 +91,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
 # ===============================
 # Database (Railway PostgreSQL)
 # ===============================
-# DATABASES = {
-#     'default': {
-#         'ENGINE': config("DB_ENGINE"),
-#         'NAME': config("DB_NAME"),
-#         'USER': config("DB_USER"),
-#         'PASSWORD': config("DB_PASSWORD"),
-#         'HOST': config("DB_HOST"),
-#         'PORT': config("DB_PORT"),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': config("DB_ENGINE"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
+    }
+}
 
 # ===============================
 # Password validation
