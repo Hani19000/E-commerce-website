3133b32378f7ede0442c73667e51555185cd58d6
Hani <hanider27@gmail.com>
2025-12-17 16:19:19 +0100

    mise a jours

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -104,10 +104,10 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database (Railway PostgreSQL)
 # ===============================
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        conn_health_checks=True,
+        conn_health_checks=True
     )
 }
 
