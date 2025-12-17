6c06d893266503e1584bcf6021df43d57fc22270
Hani <hanider27@gmail.com>
2025-12-17 16:23:13 +0100

    mise a jours

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -111,7 +111,6 @@ DATABASES = {
         'PASSWORD': config("DB_PASSWORD"),
         'HOST': config("DB_HOST"),
         'PORT': config("DB_PORT"),
-        'DATABASE_URL': config("DATABASE_URL"),
     }
 }
 
